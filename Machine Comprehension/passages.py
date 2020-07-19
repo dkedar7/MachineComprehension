670df977842c5f6a68bbe00c8930a620fc72a29e
Kedar Dabhadkar <kedar.dabhadkar@gmail.com>
2020-07-19 17:33:42 +0000

    Restructure directory

---
+++ Machine Comprehension/passages.py
@@ -0,0 +1,18 @@
+passages = {
+    'The Money Beat (Drumming)' : """Let me tell you how you can play the money beat on your drums. To get this beat going start with one part at a time. This way you can part your mind, and feel the groove a lot better. With you hi hat, play constant eight notes. We will add in some accents in the future, but for now, just play eight notes. Rememember to count out loud when you are playing, it will help you out a lot!
+    
+    Now that you have got this, try to not think about it. When people first learn to play the drums they usually think too much. This is where they start to make mistakes, they overthink. Your hi hat hand will not change this motion, so try to forget about it. Now it's time to concentrate on your other hand. With this hand, you will be playing quarter notes on the snare. These snare hits will be on the 2 and 4 count.
+    
+    Good! Now let’s finish it off with the bass drum. This too will be playing quarter notes, however, not on the 2 and four. Most beginners will have trouble with this, they will end up playing their snare and bass drum at the same time. Take your time and it will come to you. Play the bass on the 1 and 3 counts.
+""",
+    "Revel's Pitch" : "Revel is a full-service digital consultancy that partners with organizations to lead go-to-market transformation by building seamless, simple, connected experiences. We are problem solvers, designers and builders united around a core set of values and a commitment to solving complex challenges swiftly, efficiently and elegantly.",
+    
+    "Doppler Effect": """The Doppler effect (or the Doppler shift) is the change in frequency of a wave in relation to an observer who is moving relative to the wave source. It is named after the Austrian physicist Christian Doppler, who described the phenomenon in 1842.
+
+A common example of Doppler shift is the change of pitch heard when a vehicle sounding a horn approaches and recedes from an observer. Compared to the emitted frequency, the received frequency is higher during the approach, identical at the instant of passing by, and lower during the recession.
+
+The reason for the Doppler effect is that when the source of the waves is moving towards the observer, each successive wave crest is emitted from a position closer to the observer than the crest of the previous wave. Therefore, each wave takes slightly less time to reach the observer than the previous wave. Hence, the time between the arrivals of successive wave crests at the observer is reduced, causing an increase in the frequency. While they are traveling, the distance between successive wave fronts is reduced, so the waves "bunch together". Conversely, if the source of waves is moving away from the observer, each wave is emitted from a position farther from the observer than the previous wave, so the arrival time between successive waves is increased, reducing the frequency. The distance between successive wave fronts is then increased, so the waves "spread out".
+
+For waves that propagate in a medium, such as sound waves, the velocity of the observer and of the source are relative to the medium in which the waves are transmitted. The total Doppler effect may therefore result from motion of the source, motion of the observer, or motion of the medium. Each of these effects is analyzed separately. For waves which do not require a medium, such as light or gravity in general relativity, only the relative difference in velocity between the observer and the source needs to be considered.
+"""
+}
